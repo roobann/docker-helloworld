698d162352b2af439caed3fb1a9765d242f4e442
Rooban <roobanponrajn@gmail.com>
2023-09-09 09:03:58 +0530

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -2,6 +2,8 @@
 
 # Replace the hostname in the container
 sed -i.bak 's/HOSTNAME/'"$HOSTNAME"'/g' /www/data/index.html
+sed -i.bak 's/HOSTNAME/'"$HOSTNAME"'/g' /www/data/app1/index.html
+sed -i.bak 's/HOSTNAME/'"$HOSTNAME"'/g' /www/data/app2/index.html
 
 # Startup the cmd
 exec "$@"
